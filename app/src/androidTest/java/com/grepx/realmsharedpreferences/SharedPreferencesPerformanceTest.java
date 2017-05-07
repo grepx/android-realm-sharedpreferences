2d11543e87f93f19714dd45651c703ab761ce3e7
Greg Pearce <gregrpearce@gmail.com>
2017-05-07 21:07:53 +0100

    start adding in the code to test all the test cases

--- app/src/androidTest/java/com/grepx/realmsharedpreferences/SharedPreferencesPerformanceTest.java
+++ app/src/androidTest/java/com/grepx/realmsharedpreferences/SharedPreferencesPerformanceTest.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
+import java.util.HashSet;
+import java.util.Random;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,16 +19,20 @@ public class SharedPreferencesPerformanceTest {
   int readWriteType;
   int dataLocality;
   int recordCount;
+  Random random;
 
-  final String SHARED_PREF_NAME = "SharedPreferences";
-  final String REALM_PREF_NAME = "RealmSharedPreferences";
+  final String SHARED_PREFERENCES_NAME = "SharedPreferences";
+  final String REALM_PREFERENCES_NAME = "RealmSharedPreferences";
 
-  final int TYPE_READ = 0;
-  final int TYPE_WRITE = 1;
-  final int TYPE_READ_WRITE = 2;
+  final int IMPL_SHARED_PREFERENCES = 0;
+  final int IMPL_REALM = 1;
 
-  final int LOCALITY_LOW = 0;
-  final int LOCALITY_HIGH = 1;
+  final int TYPE_READ = 10;
+  final int TYPE_WRITE = 11;
+  final int TYPE_READ_WRITE = 12;
+
+  final int LOCALITY_LOW = 20;
+  final int LOCALITY_HIGH = 21;
 
   final int RECORD_COUNT_LOW = 50;
   final int RECORD_COUNT_MEDIUM = 250;
@@ -36,7 +42,7 @@ public class SharedPreferencesPerformanceTest {
   public void sharedPreferencesReadHighLocalityTest() throws Exception {
     Context context = InstrumentationRegistry.getTargetContext();
     SharedPreferences sharedPreferences =
-        context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
 
     // mixed read/write with high data locality test
     long startTime = System.nanoTime();
@@ -48,6 +54,85 @@ public class SharedPreferencesPerformanceTest {
     printElapsedTime("SharedPreferences Read HighLocality - ", startTime);
   }
 
+  private void testConfiguration(String configName, int implementation, int readWriteType,
+                                 int dataLocality, int recordCount) {
+    this.readWriteType = readWriteType;
+    this.dataLocality = dataLocality;
+    this.recordCount = recordCount;
+    // arbitrary seed to create a deterministic random number generator
+    random = new Random(8123576229384l);
+
+    SharedPreferences sharedPreferences;
+    switch (implementation) {
+      case IMPL_SHARED_PREFERENCES:
+        sharedPreferences =
+            InstrumentationRegistry.getTargetContext()
+                                   .getSharedPreferences(SHARED_PREFERENCES_NAME,
+                                                         Context.MODE_PRIVATE);
+        break;
+      case IMPL_REALM:
+        sharedPreferences = new RealmSharedPreferences(REALM_PREFERENCES_NAME);
+        break;
+      default:
+        throw new RuntimeException();
+    }
+
+    initPreferences();
+  }
+
+  private void initPreferences() {
+
+  }
+
+  private void writeRandomValue(int prefIndex) {
+    int prefType = prefIndex % 6;
+    switch (prefType) {
+      case 0:
+        sharedPreferences.edit()
+                         .putBoolean(getKey(prefIndex), random.nextBoolean())
+                         .apply();
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  }
+
+  private void readValue(int prefIndex) {
+    int prefType = prefIndex % 6;
+    switch (prefType) {
+      case 0:
+        sharedPreferences.getBoolean(getKey(prefIndex), false);
+        break;
+      case 1:
+        sharedPreferences.getFloat(getKey(prefIndex), 0f);
+        break;
+      case 2:
+        sharedPreferences.getInt(getKey(prefIndex), 0);
+        break;
+      case 3:
+        sharedPreferences.getLong(getKey(prefIndex), 0l);
+        break;
+      case 4:
+        sharedPreferences.getString(getKey(prefIndex), "");
+        break;
+      case 5:
+        sharedPreferences.getStringSet(getKey(prefIndex), new HashSet<String>());
+        break;
+    }
+  }
+
+  private String getKey(int prefIndex) {
+    return "test_key" + prefIndex;
+  }
+
   private void printElapsedTime(String configuration, long startTime) {
     Log.d(TAG, configuration + (System.nanoTime() - startTime) / 1000000l + "ms");
   }
@@ -55,10 +140,6 @@ public class SharedPreferencesPerformanceTest {
   private void writeString(int testStep) {
     //sharedPreferences.edit()
     //                 .putString()
-  }
-
-  private String getKey(int testStep) {
-    return "test_key" + testStep;
   }
 
   private String getValueString(int testStep) {
